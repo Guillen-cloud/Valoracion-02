d883ac8bea38caa89bb82b700fc7e72608823d46
Guillen-cloud <guillenbejaranor@gmail.com>
2024-12-22 13:26:16 -0400

    subida de proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { AcercadeComponent } from './pages/acercade/acercade.component';
+import { NavbarComponent } from './componets/navbar/navbar.component';
+import { FooterComponent } from './componets/footer/footer.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { ContrasenaPipe } from "./pipes/contrasena.pipe";
+import { DomseguroPipe } from "./pipes/DomSanitizer.pipe";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    AcercadeComponent,
+    NavbarComponent,
+    FooterComponent,
+    CapitalizadoPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    ContrasenaPipe,
+    DomseguroPipe
+],
   providers: [],
   bootstrap: [AppComponent]
 })
